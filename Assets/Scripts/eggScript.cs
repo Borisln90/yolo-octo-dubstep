a6292af99e7f4727d6f5a338896f5050a54b8e40
Boris Lykke Nielsen <borisln@outlook.com>
2014-03-06 14:26:45 +0100

    Eggs hide instead of destroy

--- Assets/Scripts/eggScript.cs
+++ Assets/Scripts/eggScript.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class eggScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float lifeTime = 3; 
+	public bool hidden = false;
 
-		Destroy(gameObject,3);
-	
-	}
+	// Use this for initialization
+	void Start () { }
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if ( !hidden ) {
+			lifeTime -= Time.deltaTime;
+		}
+
+		if ( lifeTime <= 0 ) {
+			this.renderer.enabled = false;
+			this.hidden = true;
+		}
 	}
 }
