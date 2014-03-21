d46bdb0fd2307dba47162223d6c5938ea3536180
Onlyskau <skau.stefan@gmail.com>
2014-03-21 16:38:39 +0100

    Added script to random placed powerUp
    still need "array" of placements. see
    
    http://answers.unity3d.com/questions/534933/random-spawn-random-prefab.html
    for inspiration

---
+++ Assets/Scripts/PowerupScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupScript : MonoBehaviour {
+
+	public Transform brick;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		Instantiate(brick, new Vector3(Random.Range(-8,8), Random.Range(-8,8), Random.Range(0,0)), Quaternion.identity); // Instantiate a prefab randomly on stage.
+		//Missing: Define specific arrays for placements, so that it doesnt end up where you cant reach it
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
