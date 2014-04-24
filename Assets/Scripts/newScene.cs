c3dda94fd6d6309795e8f09003f146e8a3d429b8
Stefan Lavlund Skau <skau.stefan@gmail.com>
2014-04-24 13:02:08 +0200

    added music

--- Assets/Scripts/newScene.cs
+++ Assets/Scripts/newScene.cs
@@ -15,8 +15,6 @@ public class newScene : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButton("Jump")){ // Space button to go to the actual game
-
-			audio.Play ();
 			Application.LoadLevel (1); // set the "game" scene to level 2. Done in build settings
 
 
