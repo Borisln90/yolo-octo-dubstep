8fd1cbd0bf93a823f4cc45e9f6e65ebcfc8816d2
Boris Lykke Nielsen <borisln@outlook.com>
2014-04-24 13:23:45 +0200

    Powerup position, music loop

--- Assets/Scripts/PowerupScript.cs
+++ Assets/Scripts/PowerupScript.cs
@@ -6,13 +6,15 @@ public class PowerupScript : MonoBehaviour {
 	public Transform brick;
 
 
-
-
-
 	// Use this for initialization
 	void Start () {
 
-		Instantiate(brick, new Vector3(Random.Range(-13,13), Random.Range(-13,13), Random.Range(0,0)), Quaternion.identity); // Instantiate a prefab randomly on stage.
+		GameObject[] boxes;
+
+		boxes = GameObject.FindGameObjectsWithTag("softBox");
+		GameObject box = boxes[Random.Range(0, boxes.Length)] as GameObject;
+
+		Instantiate(brick, box.transform.position, Quaternion.identity); // Instantiate a prefab randomly on stage.
 		//Missing: Define specific arrays for placements, so that it doesnt end up where you cant reach it
 
 
