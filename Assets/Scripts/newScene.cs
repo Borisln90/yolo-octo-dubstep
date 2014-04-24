c339994f9d17168eb2b60ff726a27dcf805d0beb
Stefan Lavlund Skau <skau.stefan@gmail.com>
2014-04-24 12:46:00 +0200

    added music and did something else :D

--- Assets/Scripts/newScene.cs
+++ Assets/Scripts/newScene.cs
@@ -1,17 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+
+
 
 public class newScene : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButton("Jump")){ // Space button to go to the actual game
+
+			audio.Play ();
 			Application.LoadLevel (1); // set the "game" scene to level 2. Done in build settings
+
+
 		}
 	
 	}
